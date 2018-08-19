e6c221389cf4cdf1288c347d3ae2828e0184ee41
nonnimartin <nonnimartin@gmail.com>
2018-08-19 15:13:39 -0700

    i had removed targetstring and that was a mistake

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function seekMatch(file, target){
 			//print test data every ~100 guesses
 			if (triesCounter % 100 == 0) console.log('100 attempts: currently trying to match pass: ' + line);
             isMatch(line, target);
-            console.log('trying to match: ' + line + ' current hash = ' + trip(line));
+            console.log('trying to match: ' + line + ' current hash = ' + trip(line) + ' to ' + target);
             //update tries counter
 			triesCounter += 1;
 			//update 'tried' array
@@ -89,5 +89,5 @@ function seekMatch(file, target){
 //iterate through dictionaries directory and 
 //run seekmatch
 fs.readdirSync('dicts').forEach(file => {
-  seekMatch('./dicts/' + file);
+  seekMatch('./dicts/' + file, targetString);
 })
